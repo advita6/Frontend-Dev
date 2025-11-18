94651bb61541634c166a5861ffeedcb1478ce30a
Advita <advita.singh_cs.ccv23@gla.ac.in>
2025-11-18 22:14:54 +0530

    Added Javascript assignments

---
+++ eg1.js
@@ -0,0 +1,96 @@
+/* let day=0;
+let dayName;
+
+switch(day)
+{
+    case 1:
+        dayName="Monday";
+    break;
+
+    case 2:
+        dayName="Tuesday";
+    break;
+
+    case 3:
+        dayName="Wednesday";
+    break;
+
+    case 4:
+        dayName="Thursday";
+    break;
+
+    case 5:
+        dayName="Friday";
+    break;
+
+    case 6:
+        dayName="Saturday";
+    break;
+
+    case 7:
+        dayName="Sunday";
+        break;
+
+    default:
+        dayName="Invalid day";
+
+}
+
+console.log(`Today is ${dayName}`);
+
+
+
+ */
+/* for(let i=0;i<=10;i++)
+{
+    console.log(i);
+}
+
+console.log("This is while loop")
+let i=0;
+while(i<=10)
+{
+    console.log(i);
+    i++;
+} */
+
+
+/* let colors=["blue", "red", "green"]
+    for(let color of colors)
+    {
+        console.log(color);
+    }  */
+
+/* let car=
+{
+    brand: "ABC",
+    model: "XYZ",
+    year: 2027
+};
+
+for(let key in car)
+{
+    console.log(`${key}: ${car[key]}`);
+
+} */
+
+
+/* let result="Hello" + 5;
+console.log(result);
+console.log(typeof result); */
+
+
+let result1=10 - "4"
+console.log(result1);
+console.log(typeof result1)
+
+let result2="Value is" + true
+console.log(result2);
+
+try{
+    console.log(10/0)
+    console.log(x)
+} catch(error)
+{
+    console.log("An error occurred: " + console.log(console.log(error)));
+}
